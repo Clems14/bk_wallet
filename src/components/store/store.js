7ece9f78497790edbd4b15d75786d795ea735430
fournici <francaisdelarue98@gmail.com>
2019-05-21 16:36:42 +0700

    fix of axios problem (protocol)

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(Vuex);
-Vue.use(Axios);
+Vue.use(VueAxios, Axios);
 
 
 export const store = new Vuex.Store({
@@ -21,6 +22,9 @@ export const store = new Vuex.Store({
 			})
 							.then((response) => {
 								console.log(response);
+								// console.log(response.data.detail.token);
+								this.loginToken = response.data.detail.token;
+								console.log(this.loginToken);
 								/*if(response.data.status === 'fail'){
 									alert('Authentification fail');
 									window.location.href='./sign_in.html'
